95762166647d50467a9c2c5d0b0a8c9ba4c361f0
majisyou <majisyou1999@gmail.com>
2022-03-19 15:45:11 +0900

    commit

--- src/main/java/com/github/majisyou/dice/Dice.java
+++ src/main/java/com/github/majisyou/dice/Dice.java
@@ -1,16 +1,21 @@
 package com.github.majisyou.dice;
 
+import com.github.majisyou.dice.commands.C_locommand;
+import com.github.majisyou.dice.commands.DiceCommand;
 import com.github.majisyou.dice.commands.DiceTest;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Dice extends JavaPlugin {
+    private static Plugin plugin;
 
     @Override
     public void onEnable() {
         getLogger().info("Diceプラグインジェネレート");
-        getCommand("dice").setExecutor(new Dice());
-        getCommand("dicetest").setExecutor(new DiceTest());
+        getCommand("setdice").setExecutor(new DiceCommand());
+        getCommand("setdicetest").setExecutor(new DiceTest());
+        getCommand("setc_lo").setExecutor(new C_locommand());
 
 
 //        //ここは定型文
@@ -24,5 +29,9 @@ public final class Dice extends JavaPlugin {
     public void onDisable() {
         getLogger().info("Diceプラグインが停止した");
         // Plugin shutdown logic
+    }
+
+    public static Plugin getPlugin(){   //この意味わからない
+        return plugin;
     }
 }
--- src/main/java/com/github/majisyou/dice/system/Dicesystem.java
+++ src/main/java/com/github/majisyou/dice/system/Dicesystem.java
@@ -7,28 +7,17 @@ import java.util.logging.Logger;
 
 public class Dicesystem {
 
-    public static int[] Dicemain(String[] args) {
+    public static int[] Dicemain(int times,int number) {
 
         int[] resultdice = new int[0];  //初期化と呼び出し
 
-        if (args.length == 3) {
-            if (!(args[2].chars().allMatch(Character::isDigit)) || !(args[1].chars().allMatch(Character::isDigit))) {
-                System.out.println("引数1と引数2は数字で代入してください");
-            }
-            String stringdicetime = args[1];
-            String stringdicenumber = args[2];
-            int times = Byte.parseByte(stringdicetime);
-            int number = Byte.parseByte(stringdicenumber);
             int result;
-            resultdice = new int[number];
+            resultdice = new int[times];
             Random ranNum = new Random();
             for (int i = 0; i < times; i++) {
-                result = ranNum.nextInt(number);
+                result = ranNum.nextInt(number) + 1;
                 resultdice[i] = result;
             }
-        } else {
-            System.out.println("引数1はダイスを振る回数,引数2はダイスの面の数,引数3は意味なし");
-        }
 
         return resultdice;
 
